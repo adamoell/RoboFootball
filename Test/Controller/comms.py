66c1839b7703471a4164816271f4d948ca44a285
adamoell <adam@oellermann.com>
2024-06-07 11:49:25 +0100

    Updated copyright headers

--- Test/Controller/comms.py
+++ Test/Controller/comms.py
@@ -1,4 +1,23 @@
 # comms.py
+'''
+comms.py: ESP-NOW based comms library for robot and controller
+Copyright (C) 2024 by Adam Oellermann (adam@oellermann.com)
+--------------------------------------------------------------------------------
+This file is part of RoboFootball.
+
+RoboFootball is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+RoboFootball is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+RoboFootball. If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import network
 import espnow
 import binascii
